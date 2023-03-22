43a05611a80ba474ff7b9b40c165e954199782bf
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-03-21 18:34:20 -0600

    Service Prefix

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ from google.cloud import secretmanager
 from pydantic import BaseSettings
 
 PROJECT_ID = os.getenv("PROJECT_ID", "")
+SERVICE_PREFIX = os.getenv("SERVICE_PREFIX", "pjecz_plataforma_web_api")
 
 
 def get_secret(secret_id: str) -> str:
@@ -23,7 +24,8 @@ def get_secret(secret_id: str) -> str:
     client = secretmanager.SecretManagerServiceClient()
 
     # Build the resource name of the secret version
-    name = client.secret_version_path(PROJECT_ID, f"pjecz_plataforma_web_{secret_id}", "latest")
+    secret = f"{SERVICE_PREFIX}_{secret_id}"
+    name = client.secret_version_path(PROJECT_ID, secret, "latest")
 
     # Access the secret version
     response = client.access_secret_version(name=name)
@@ -36,7 +38,7 @@ class Settings(BaseSettings):
     """Settings"""
 
     db_host: str = get_secret("db_host")
-    db_port: int = 5432
+    db_port: int = get_secret("db_port")
     db_name: str = get_secret("db_name")
     db_pass: str = get_secret("db_pass")
     db_user: str = get_secret("db_user")
