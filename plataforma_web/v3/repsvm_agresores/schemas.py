25cd6486b59721e62a453b81e6367068e8d7f722
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-03-29 15:45:04 -0600

    Correccion schema

--- plataforma_web/v3/repsvm_agresores/schemas.py
+++ plataforma_web/v3/repsvm_agresores/schemas.py
@@ -1,8 +1,6 @@
 """
 REPSVM Agresores v3, esquemas de pydantic
 """
-from datetime import date
-
 from pydantic import BaseModel
 
 from lib.schemas_base import OneBaseOut
@@ -18,9 +16,14 @@ class RepsvmAgresorOut(BaseModel):
     distrito_nombre_corto: str | None
     consecutivo: int | None
     delito_generico: str | None
+    delito_especifico: str | None
     nombre: str | None
+    numero_causa: str | None
     pena_impuesta: str | None
-    sentente_url: str | None
+    observaciones: str | None
+    sentencia_url: str | None
+    tipo_juzgado: str | None
+    tipo_sentencia: str | None
 
     class Config:
         """SQLAlchemy config"""
