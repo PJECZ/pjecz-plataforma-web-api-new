5f63d7c27f7eec99de07f057fb9b6a683ddf16da
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-04-01 22:10:09 -0600

    Peritos esquemas.

---
+++ plataforma_web/v3/peritos/schemas.py
@@ -0,0 +1,31 @@
+"""
+Peritos v3, esquemas de pydantic
+"""
+from pydantic import BaseModel
+
+from lib.schemas_base import OneBaseOut
+
+
+class PeritoOut(BaseModel):
+    """Esquema para entregar peritos"""
+
+    id: int | None
+    distrito_id: int | None
+    distrito_clave: str | None
+    distrito_nombre: str | None
+    distrito_nombre_corto: str | None
+    nombre: str | None
+    domicilio: str | None
+    telefono_fijo: str | None
+    telefono_celular: str | None
+    email: str | None
+    notas: str | None
+
+    class Config:
+        """SQLAlchemy config"""
+
+        orm_mode = True
+
+
+class OnePeritoOut(PeritoOut, OneBaseOut):
+    """Esquema para entregar un perito"""
