ad92c3d7319dcb08c6150556e5efbdf1c5b0a8e8
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-04-01 23:49:33 -0600

    Correccion Black

--- plataforma_web/v3/peritos_tipos/schemas.py
+++ plataforma_web/v3/peritos_tipos/schemas.py
@@ -7,16 +7,16 @@ from lib.schemas_base import OneBaseOut
 
 
 class PeritoTipoOut(BaseModel):
-    """ Esquema para entregar tipos de peritos """
+    """Esquema para entregar tipos de peritos"""
 
     id: int | None
     nombre: str | None
 
     class Config:
-        """ SQLAlchemy config """
+        """SQLAlchemy config"""
 
         orm_mode = True
 
 
 class OnePeritoTipoOut(PeritoTipoOut, OneBaseOut):
-    """ Esquema para entregar un tipo de perito """
+    """Esquema para entregar un tipo de perito"""
