be94bbb10ca39d02b2e7fb4e86c6d8a6535ffd23
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-04-03 17:44:10 -0600

    Tesis Jurisprudencias crud

---
+++ plataforma_web/v3/epocas/crud.py
@@ -0,0 +1,25 @@
+"""
+Epocas v3, CRUD (create, read, update, and delete)
+"""
+from typing import Any
+
+from sqlalchemy.orm import Session
+
+from lib.exceptions import MyIsDeletedError, MyNotExistsError
+
+from ...core.epocas.models import Epoca
+
+
+def get_epocas(db: Session) -> Any:
+    """Consultar las epocas activas"""
+    return db.query(Epoca).filter_by(estatus="A").order_by(Epoca.nombre)
+
+
+def get_epoca(db: Session, epoca_id: int) -> Epoca:
+    """Consultar una epoca por su id"""
+    epoca = db.query(Epoca).get(epoca_id)
+    if epoca is None:
+        raise MyNotExistsError("No existe ese epoca")
+    if epoca.estatus != "A":
+        raise MyIsDeletedError("No es activo ese epoca, está eliminado")
+    return epoca
