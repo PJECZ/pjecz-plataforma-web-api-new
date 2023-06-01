ce256a771a33c64adc85386f8592edb2b2d06906
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-06-01 16:57:32 -0600

    Revision a safe string

--- lib/safe_string.py
+++ lib/safe_string.py
@@ -6,6 +6,11 @@ import re
 
 from unidecode import unidecode
 
+CURP_REGEXP = r"^[A-Z]{4}\d{6}[A-Z]{6}[A-Z0-9]{2}$"
+EMAIL_REGEXP = r"^[\w.-]+@[\w.-]+\.\w+$"
+PASSWORD_REGEXP = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,24}$"
+TELEFONO_REGEXP = r"^[1-9]\d{9}$"
+
 
 def safe_clave(input_str):
     """Safe clave"""
@@ -18,28 +23,43 @@ def safe_clave(input_str):
     return new_string
 
 
-def safe_string(input_str, max_len=250):
-    """Safe string"""
+def safe_curp(input_str):
+    """Safe CURP"""
     if not isinstance(input_str, str):
-        return ""
-    new_string = re.sub(r"[^a-zA-Z0-9,/-]+", " ", unidecode(input_str))
-    removed_multiple_spaces = re.sub(r"\s+", " ", new_string)
-    final = removed_multiple_spaces.strip().upper()
-    return (final[:max_len] + "...") if len(final) > max_len else final
+        raise ValueError("CURP no es texto")
+    removed_spaces = re.sub(r"\s", "", input_str)
+    removed_simbols = re.sub(r"[()\[\]:/.-]+", "", removed_spaces)
+    final = unidecode(removed_simbols.upper())
+    if re.fullmatch(CURP_REGEXP, final) is None:
+        raise ValueError("CURP es incorrecto")
+    return final
+
+
+def safe_email(input_str, search_fragment=False):
+    """Safe email"""
+    if not isinstance(input_str, str) or input_str.strip() == "":
+        raise ValueError("Email es incorrecto")
+    final = input_str.strip().lower()
+    if search_fragment:
+        if re.match(r"^[\w.-]*@*[\w.-]*\.*\w*$", final) is None:
+            return None
+        return final
+    regexp = re.compile(EMAIL_REGEXP)
+    if regexp.match(final) is None:
+        raise ValueError("Email es incorrecto")
+    return final
 
 
 def safe_expediente(input_str):
     """Safe expediente"""
     if not isinstance(input_str, str) or input_str.strip() == "":
-        return ""
+        raise ValueError("Expediente es incorrecto")
     elementos = re.sub(r"[^a-zA-Z0-9]+", "|", unidecode(input_str)).split("|")
     try:
         numero = int(elementos[0])
         ano = int(elementos[1])
     except (IndexError, ValueError) as error:
         raise error
-    if numero <= 0:
-        raise ValueError
     if ano < 1950 or ano > date.today().year:
         raise ValueError
     extra_1 = ""
@@ -52,3 +72,23 @@ def safe_expediente(input_str):
     if len(limpio) > 16:
         raise ValueError
     return limpio
+
+
+def safe_string(input_str, max_len=250):
+    """Safe string"""
+    if not isinstance(input_str, str):
+        return ""
+    new_string = re.sub(r"[^a-zA-Z0-9,/-]+", " ", unidecode(input_str))
+    removed_multiple_spaces = re.sub(r"\s+", " ", new_string)
+    final = removed_multiple_spaces.strip().upper()
+    return (final[:max_len] + "...") if len(final) > max_len else final
+
+
+def safe_telefono(input_str):
+    """Safe telefono"""
+    if not isinstance(input_str, str):
+        raise ValueError("Telefono no es texto")
+    solo_numeros = re.sub(r"[^0-9]+", "", unidecode(input_str))
+    if re.match(TELEFONO_REGEXP, solo_numeros) is None:
+        raise ValueError("Telefono está incompleto")
+    return solo_numeros
