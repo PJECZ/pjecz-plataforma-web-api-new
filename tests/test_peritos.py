adc6bb66492b9bd97da2b27305102c2d02725235
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-06-28 15:31:22 -0600

    Filtro expediente en edictos

--- tests/test_peritos.py
+++ tests/test_peritos.py
@@ -35,6 +35,21 @@ class TestPeritos(unittest.TestCase):
         for item in result["items"]:
             self.assertEqual(item["distrito_id"], 6)
 
+    def test_get_peritos_by_distrito_clave_trc(self):
+        """Test GET method for peritos by distrito_clave DTRC"""
+        response = requests.get(
+            f"{config['host']}/v3/peritos",
+            headers={"X-Api-Key": config["api_key"]},
+            params={"distrito_clave": "DTRC"},
+            timeout=config["timeout"],
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["success"], True)
+        result = data["result"]
+        for item in result["items"]:
+            self.assertEqual(item["distrito_clave"], "DTRC")
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_repsvm.py
+++ tests/test_repsvm.py
@@ -20,6 +20,36 @@ class TestREPSVM(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
+    def test_get_repsvm_by_distrito_id_6(self):
+        """Test GET method for repsvm_agresores by distrito_id 6"""
+        response = requests.get(
+            f"{config['host']}/v3/repsvm_agresores",
+            headers={"X-Api-Key": config["api_key"]},
+            params={"distrito_id": 6},
+            timeout=config["timeout"],
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["success"], True)
+        result = data["result"]
+        for item in result["items"]:
+            self.assertEqual(item["distrito_id"], 6)
+
+    def test_get_repsvm_by_distrito_clave_dtrc(self):
+        """Test GET method for repsvm_agresores by distrito_clave DTRC"""
+        response = requests.get(
+            f"{config['host']}/v3/repsvm_agresores",
+            headers={"X-Api-Key": config["api_key"]},
+            params={"distrito_clave": "DTRC"},
+            timeout=config["timeout"],
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["success"], True)
+        result = data["result"]
+        for item in result["items"]:
+            self.assertEqual(item["distrito_clave"], "DTRC")
+
 
 if __name__ == "__main__":
     unittest.main()
